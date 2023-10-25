7710e7ef63bee898e584e9b663aceddb971f2e16
chollweg <cassio-hollweg1@educar.rs.gov.br>
2023-10-25 20:00:57 -0300

    segundo commit

---
+++ src/models/DatabaseModel.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+async function connect(){
+  if(global.connection && global.connection.state !== 'disconnected')
+      return global.connection;
+    const mysql = require("mysql2/promise");
+   
+    const connection = await mysql.createConnection({
+      host: process.env.HOST,
+      port:process.env.PORT,
+      user: process.env.USER,
+      password : process.env.PASSWORD,
+      database: process.env.DATABASE      
+    });
+  console.log("Conectou no MySQL!");
+  global.connection = connection;
+  return connection;
+}
